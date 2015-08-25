2878693d4dad5aa7279be4860c23d77f6b901451
David Duque Fuertes <david.duque.f@gmail.com>
2015-08-25 19:05:52 +0200

    - Change Spotify App by Popular Movies App (the corresponding to my Nanodegree cohort: july 2015) - Delete unnecessary menu_main for MainActivity - Do some refactoring and improvements - Enable launching Popular Movies App from its corresponding button

--- app/src/main/java/com/udacity/nanodegree/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/udacity/nanodegree/android/myappportfolio/MainActivity.java
@@ -1,15 +1,20 @@
 package com.udacity.nanodegree.android.myappportfolio;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,30 +22,46 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void launchSpotifyApp (View view) {
-        this.showToast("This button will launch my " + this.getResources().getString(R.string.spotify_app_name) + " app!");
+    public void launchNanodegreeApp(View view) {
+        String appPackageName;
+        switch (view.getId()) {
+            case R.id.buttonPopularMovies:
+                appPackageName = "com.udacity.nanodegree.android.popularmovies";
+                break;
+            case R.id.buttonScores:
+            case R.id.buttonLibrary:
+            case R.id.buttonBuildItBigger:
+            case R.id.buttonXYZ:
+            case R.id.buttonCapstone:
+            default:
+                appPackageName = null;
+                break;
+        }
+        if (appPackageName != null ) {
+            // Actually launch the selected app
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setComponent(new ComponentName(appPackageName,
+                    appPackageName + ".MainActivity"));
+            launchNewApp(intent);
+        } else {
+            // Temporarily, show a Toast with the app name
+            if (view instanceof Button) {
+                showToast("This button will launch my " + ((Button)view).getText().toString() +
+                        " app!");
+            }
+        }
     }
 
-    public void launchScoresApp (View view) {
-        this.showToast("This button will launch my " + this.getResources().getString(R.string.scores_app_name) + " app!");
+    private void launchNewApp (Intent intent) {
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            Log.w(LOG_TAG, "Required activity (" + intent.getComponent().getClassName() +
+                            ") could not be found");
+            showToast(getString(R.string.app_not_found));
+        }
     }
-
-    public void launchLibraryApp (View view) {
-        this.showToast("This button will launch my " + this.getResources().getString(R.string.library_app_name) + " app!");
-    }
-
-    public void launchBuildItBiggerApp (View view) {
-        this.showToast("This button will launch my " + this.getResources().getString(R.string.build_it_bigger_app_name) + " app!");
-    }
-
-    public void launchXYZReaderApp (View view) {
-        this.showToast("This button will launch my " + this.getResources().getString(R.string.xyz_reader_app_name) + " app!");
-    }
-
-    public void launchCapstoneApp (View view) {
-        this.showToast("This button will launch my " + this.getResources().getString(R.string.capstone_app_name) + " app!");
-    }
-
     private void showToast (String message) {
 
         Context context = this.getApplicationContext();
